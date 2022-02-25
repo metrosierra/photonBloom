dd1ef88bf14f8140d84084556b97b2812b8236fb
Wu Mingsong <mingsong123@gmail.com>
2022-02-25 13:20:13 +0000

    bin combing method rough draft

--- cross_corr_subroutine.py
+++ cross_corr_subroutine.py
@@ -8,32 +8,97 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from numba import njit, prange
+from natsort import natsorted
+import os
+
+import mathematics as mathy
+
+
+@njit
+def signal_bin_combing(data, bin_width, sn_bin_no = (1, 99)):
+
+    total_time = np.max(data) - np.min(data)
+    bin_no = int(np.floor(total_time / bin_width))
+
+    signal_no = sn_bin_no[0]
+    noise_no = sn_bin_no[1]
+    period_no = int(signal_no + noise_no)
+
+    period_cycles = int(np.floor(bin_no / period_no))
+    print(period_cycles, 'cycles')
+    counts, edges = mathy.numba_histogram(data, bin_no)
+
+    output_data = []
+    for i in range(period_no*2):
+
+        sig_count = 0
+        for q in range(period_cycles-1):
+
+            start_index = q*(period_no-1) + i
+            sig_count += np.sum(counts[start_index : start_index + signal_no])
+
+        output_data.append(sig_count / period_cycles)
+    # print(output_data)
+    return output_data
 
 
 
-@njit(parallel = True)
-def delay_cross_correlation(master, slave, max_delay = -1):
+data_dir = 'data/'
+files = natsorted(os.listdir(data_dir))
 
-    length_master = len(master)
-    length_slave = len(slave)
-    delays = []
+###because of natsorted (natural sort), collected_blah.npy should be
+###in front of tags_blah.npy because alphabetical order
+
+background_tags = np.load(data_dir + files[0])
+tags_channel_list = np.load(data_dir + files[1])
+
+channel1 = []
+channel2 = []
+
+for index, tag in enumerate(background_tags):
+
+    if tags_channel_list[index] == 1.:
+        channel1.append(tag)
+    if tags_channel_list[index] == 2.:
+        channel2.append(tag)
+
+channel1 = np.array(channel1)
+channel2 = np.array(channel2)
+
+# print(channel1[:100], channel2[:100])
+
+# #%%%%%%%%%%%%%%%%%%
+#
+# output1 = signal_bin_combing(channel1[:100000], bin_width = 0.5e5, sn_bin_no = (50, 450))
+# output2 = signal_bin_combing(channel2[:100000], bin_width = 0.5e5, sn_bin_no = (50, 450))
+#
+# output1 = np.array(output1)
+# output2 = np.array(output2)
+# plt.plot(output1, label = 'channel1')
+# plt.plot(output2, label = 'channel2')
+# plt.legend()
+# plt.xlabel('Combing index')
+# plt.ylabel('Average signal bin count')
+# plt.savefig('output/duochannel_fine_combing_test1.eps', bbox = 'tight')
+# plt.show()
+
+#%%%%%%%%%%%%%%%%%%
+output1 = signal_bin_combing(channel1[:100000], bin_width = 0.5e5, sn_bin_no = (50, 450))
+output2 = signal_bin_combing(channel1[100000:200000], bin_width = 0.5e5, sn_bin_no = (50, 450))
+
+output1 = np.array(output1)
+output2 = np.array(output2)
+plt.xlabel('Combing index')
+plt.ylabel('Average signal bin count')
+plt.plot(output1, label = 'channel1_firsthalf')
+plt.savefig('output/channel11_fine_combing_periodicity_test1.eps', bbox = 'tight')
+plt.show()
+plt.legend()
+plt.xlabel('Combing index')
+plt.ylabel('Average signal bin count')
+plt.plot(output2, label = 'channel1_secondhalf')
+plt.legend()
+plt.savefig('output/channel12_fine_combing_periodicity_test1.eps', bbox = 'tight')
 
 
-
-    origin = master[0]
-    master = master - origin
-    slave = slave - origin
-    # print(master)
-    # print(slave)
-
-    for i in range(length_master):
-        ref = master[i]
-        output = slave - ref
-        temp = output[output < max_delay]
-        temp = temp[temp > -max_delay]
-        delays.append(output)
-
-
-
-
-    return delays
+# plt.show()
--- mathematics.py
+++ mathematics.py
@@ -8,11 +8,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+from numba import njit
 
 def gaussian(p, x):
     """
     Returns a scaled Gaussian function for visual comparison of the measured data
     """
     mu, sigma, A = p
-
     return A/(np.sqrt(2*np.pi)*sigma)*np.exp(-0.5*(x-mu)**2/sigma**2)
+
+
+
+
+
+@njit
+def get_bin_edges(a, bins):
+    bin_edges = np.zeros((bins+1,), dtype=np.float64)
+    a_min = a.min()
+    a_max = a.max()
+    delta = (a_max - a_min) / bins
+    for i in range(bin_edges.shape[0]):
+        bin_edges[i] = a_min + i * delta
+
+    bin_edges[-1] = a_max  # Avoid roundoff error on last point
+    return bin_edges
+
+
+@njit
+def compute_bin(x, bin_edges):
+    # assuming uniform bins for now
+    n = bin_edges.shape[0] - 1
+    a_min = bin_edges[0]
+    a_max = bin_edges[-1]
+
+    # special case to mirror NumPy behavior for last bin
+    if x == a_max:
+        return n - 1 # a_max always in last bin
+
+    bin = int(n * (x - a_min) / (a_max - a_min))
+
+    if bin < 0 or bin >= n:
+        return None
+    else:
+        return bin
+
+
+@njit
+def numba_histogram(a, bins):
+    hist = np.zeros((bins,), dtype=np.intp)
+    bin_edges = get_bin_edges(a, bins)
+
+    for x in a.flat:
+        bin = compute_bin(x, bin_edges)
+        if bin is not None:
+            hist[int(bin)] += 1
+
+    return hist, bin_edges
