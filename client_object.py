34d0662688ae1db67ef2c286d49aed6289ee88a7
ecryerjenkins <evan.cryer-jenkins20@imperial.ac.uk>
2022-07-05 17:04:47 +0100

    (client_object) pass kwargs to BaseTag

--- client_object.py
+++ client_object.py
@@ -4,7 +4,7 @@ import TimeTagger
 from big_tagger import BaseTag
 
 class TagClient(BaseTag):
-    def __init__(self, ip_address = '192.168.0.2'):
+    def __init__(self, ip_address = '192.168.0.2', **kwargs):
 
         self.target_ip = ip_address
         print("Search for Time Taggers on the network...")
@@ -16,7 +16,7 @@ class TagClient(BaseTag):
 
         self.client = self.server_handshake()
 
-        super().__init__(self.client)
+        super().__init__(self.client, **kwargs)
         print('\nTimetagger object initialising...assuming it is reading from PhotonSpot nanowire single-photon detector...will prompt about detector gain settings in a bit\n')
         self.get_methods_naive()
         # print('Here are the available class methods to be used in interactive mode')
@@ -36,12 +36,12 @@ class TagClient(BaseTag):
         except RuntimeError:
             raise Exception('No Time Tagger server available on {} and the default port 41101.'.format(self.target_ip))
 
-        self.client = TimeTagger.createTimeTaggerNetwork(self.target_ip)
+        client = TimeTagger.createTimeTaggerNetwork(self.target_ip)
         print('\nConnecting to the server on localhost.')
         print('Server handshake successful!!!!!!!!!!!\n')
         self.get_info()
 
-        return self.client
+        return client
 
 
 if __name__ == '__main__':
