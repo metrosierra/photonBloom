086bd47d2ed36d04bf1b348d8bcc37bd4976654b
Photonics PC <noemail@ic.ac.uk>
2023-03-10 11:25:00 +0000

    bug fixes for sweepcorr, new live plot mode

--- datalogger_mark2.py
+++ datalogger_mark2.py
@@ -243,13 +243,13 @@ class Lotus():
             return trigcorr
 
 
-    def tag_sweep_correlation(self, startfor = -1, channels = [2,3,4], binwidth_ns = 2, n = 6000, step_no = 5, save = False):
+    def tag_sweep_correlation(self, startfor = -1, channels = [2,3,4], binwidth_ns = 2, n = 6000, step_no = 5, gatewindow_ns = 75e6, save = False):
 
         if startfor == -1:
             print('Persisting SweepXCorrelation measurement class! Close live plot to exit this!!')
             self.spot0.sweep_corr_running.append(True)
             identity = len(self.spot0.sweep_corr_running) - 1
-            threading.Thread(target = self.spot0.get_sweep_correlation, args = (startfor, channels, binwidth_ns, n, step_no, identity), daemon = True).start()
+            threading.Thread(target = self.spot0.get_sweep_correlation, args = (startfor, channels, binwidth_ns, n, step_no, gatewindow_ns, identity), daemon = True).start()
 
             qthread_args = {
                 'data_func': self.spot0.return_sweep_corr_counts, 
@@ -258,7 +258,7 @@ class Lotus():
                 'plot_no': step_no
             }
 
-            self.rose0.new_window(refresh_interval = 0.1, 
+            self.rose0.new_multiwindow(refresh_interval = 0.1, 
                                     title = 'Swept Cross Correlation Plot', 
                                     xlabel = 'Delay', 
                                     ylabel = 'Counts', 
@@ -267,13 +267,13 @@ class Lotus():
             return
 
         elif startfor > 0.:
-            sweepcorr = self.spot0.get_sweep_correlation(startfor, channels, binwidth_ns, n, step_no)
+            sweepcorr = self.spot0.get_sweep_correlation(startfor, channels, binwidth_ns, n, step_no, gatewindow_ns)
 
             if save:
                 if not os.path.exists('output/'): os.makedir('output/')
                 now = datetime.now()
                 dt_string = now.strftime("%d%m%Y_%H_%M_%S")
-                np.save('output/sweepcorrelated_width{}ns_n{}_ch{}_{:.1e}time_{}'.format(binwidth_ns, n, channels, startfor, dt_string), sweepcorr)
+                np.save('output/sweepcorrelated_width{}ns_n{}_gate{}ns_ch{}_{:.1e}time_{}'.format(binwidth_ns, n, gatewindow_ns, channels, startfor, dt_string), sweepcorr)
             return sweepcorr
 
 
--- liveplotter_mark2.py
+++ liveplotter_mark2.py
@@ -79,7 +79,7 @@ class PetalWindow(QtWidgets.QWidget):
         for i in range(self.plot_no):
             ### setting pen as integer makes line colour cycle through 9 hues by default
             ### check pyqtgraph documentation on styling...it's quite messy
-            self.plots.append(self.graph.plot(pen = i, name = 'Channel {}!!!'.format(i)))
+            self.plots.append(self.graph.plot(pen = i, name = 'Channel {}!!!'.format(i+1)))
             self.data_store.append(self.initial_xydata)
         
         self.worker = WorkerBee(data_func, data_kill_func, self.window.isHidden, self.refresh_interval, self.identity)
@@ -107,12 +107,98 @@ class PetalWindow(QtWidgets.QWidget):
     ### the idea is self.data_store is easier to access to check
     def set_data(self, data):
         
+        if self.plot_no != 5:
+            for i in range(self.plot_no):
+                self.data_store[i] = data[i]
+                self.plots[i].setData(np.arange(len(data[i])), data[i])
+        
+        else:
+            for i in range(5):
+                self.data_store[-i] = data[-i]
+                self.plots[i].setData(np.arange(len(data[-i])), data[-i])
+
+        return self
+
+
+
+class PetalWindowMulti(QtWidgets.QWidget):
+
+    ### this is link to the parent class decorated functions
+    closed = QtCore.pyqtSignal()
+
+    def __init__(self, data_func, data_kill_func, title = 'Live Plot', xlabel = 'X axis', ylabel = 'Y axis', refresh_interval = 0.1, plot_no = 1, identity = 0):
+        
+        super().__init__()
+        self.plot_no = plot_no 
+
+        self.window = pg.GraphicsLayoutWidget(show = True, title = "Live Plotting Window")
+        self.window.resize(900,500)
+        # just antialiasing
+        pg.setConfigOptions(antialias = True)
+        # Creates graph object
+        self.graphs = []
+        self.plots = []
+
+        for i in range(self.plot_no):  
+            self.graphs.append(self.window.addPlot(title = title))
+            self.graphs[i].addLegend()
+            self.graphs[i].showGrid(x = True, y = True)
+            self.plots.append(self.graphs[i].plot(pen = i, name = 'Channel {}!!!'.format(i+1)))
+
+
+        self.identity = identity
+        self.refresh_interval = refresh_interval
+        ##################### style points #####################
+        # self.graph.showGrid(x = True, y = True)
+        self.styling = {'font-size':'20px'}
+        ########################################################
+        self.initial_xydata = [[[0.], [0.]]]
+        self.xlabel = xlabel 
+        self.ylabel = ylabel
+        self.set_xlabel(self.xlabel)
+        self.set_ylabel(self.ylabel)
+
+        # creating maybe multiple line plot subclass objects for the self.graph object, store in list
+        self.data_store = [[]]
+        ### storing lineplot instances into list, with indexed data store list
+        for i in range(self.plot_no):
+            ### setting pen as integer makes line colour cycle through 9 hues by default
+            ### check pyqtgraph documentation on styling...it's quite messy
+            self.plots.append(self.graphs[i].plot(pen = i, name = 'Channel {}!!!'.format(i+1)))
+            self.data_store.append(self.initial_xydata)
+        
+        self.worker = WorkerBee(data_func, data_kill_func, self.window.isHidden, self.refresh_interval, self.identity)
+        self.make_connection(self.worker)
+        self.worker.start()
+        # self.show()
+        print('New Plot Window Identity is', self.identity)
+
+    def make_connection(self, data_object):
+        data_object.signal.connect(self.update)
+        return self
+    @QtCore.pyqtSlot(np.ndarray)
+    def update(self, data):
+        self.set_data(data)
+        return self
+    def set_xlabel(self, label):
+        for i in range(self.plot_no):
+            self.graphs[i].setLabel('bottom', label, **self.styling)
+        return self
+    def set_ylabel(self, label):
+        for i in range(self.plot_no):
+            self.graphs[i].setLabel('left', label, **self.styling)
+        return self
+    ### please input data as list of lists => [[xdata], [ydata]]
+    ### this is a local data storage, the update function then
+    ### updates the curve instances
+    ### the idea is self.data_store is easier to access to check
+    def set_data(self, data):
+        
         for i in range(self.plot_no):
             self.data_store[i] = data[i]
             self.plots[i].setData(np.arange(len(data[i])), data[i])
         
         return self
-
 
 ### Rose is the plot GUI instance, 
 ### We have one rose and multiple petals (plot windows)
@@ -134,6 +220,10 @@ class RoseApp(QtWidgets.QMainWindow):
         # self.windows[self.window_count].show()
         self.window_count += 1
 
+    def new_multiwindow(self, data_func, data_kill_func, refresh_interval = 0.0001, title = 'Live Plot', xlabel = 'X axis', ylabel = 'Y axis', plot_no = 1, identity = 0):
+        self.windows.append(PetalWindowMulti(data_func, data_kill_func, title, xlabel, ylabel, refresh_interval, plot_no, identity))
+        # self.windows[self.window_count].show()
+        self.window_count += 1
 
 ## Start Qt event loop unless running in interactive mode
 if __name__ == '__main__':
